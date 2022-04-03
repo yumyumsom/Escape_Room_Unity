8fe360c1c75c1f314da3eb54b48586dd30ded100
yumyumsom <woosw13@gmail.com>
2022-04-03 15:04:29 +0900

    modified playermove.cs

--- 0401/Assets/Script/player/playerMove.cs
+++ 0401/Assets/Script/player/playerMove.cs
@@ -3,34 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 public class playerMove : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    public float rotationSpeed = 2.0f;
-    public Rigidbody body;
-
+    private float moveSpeed = 3.5f;
+    private float rotationSpeed = 100f;
+    private float hRotationSpeed = 100f;  // 좌우 회전 속도
     void Start()
     {
-        //body = GetComponent<Rigidbody>();
-        // 중력해제
-       //body.useGravity = false;
-    }
 
-  
+    }
     void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(h, 0, v);
-
-        if (!(h == 0 && v == 0))
-        {
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            
-                angle = angle * rotationSpeed * Time.deltaTime;
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotationSpeed);
-            
-        }
-        //transform.Translate(direction * moveSpeed * Time.deltaTime);
-        //transform.position += direction * moveSpeed * Time.deltaTime;
+        float t = Time.deltaTime;
+        float h = Input.GetAxis("Horizontal") * rotationSpeed * t;// 키보드 입력 감지(좌 우 일때만 회전)
+        Quaternion hRot = Quaternion.AngleAxis(h, Vector3.up); // 회전 변위 생성
+        transform.rotation = hRot * transform.rotation;
+        Vector3 move =
+           transform.forward * Input.GetAxis("Vertical") +
+           transform.right * Input.GetAxis("Horizontal");
+        // 이동량을 좌표에 반영
+        transform.position += move * moveSpeed * t;
     }
-
 }
